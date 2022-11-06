ec5fcb71c324df1927be1ec0e91b59c70591f837
NikhoC1 <110102550+NikhoC1@users.noreply.github.com>
2022-11-06 17:11:02 +0700

    Add files via upload

---
+++ video-9/array.php
@@ -0,0 +1,45 @@
+<?php
+
+// Array dimensi
+
+$nama= array("Satu" , "Dua" , "tiga" , "empat" , 5 , 5.5);
+
+var_dump($nama);
+
+echo "<br>";
+
+echo $nama [5];
+
+echo "<br>";
+
+// for ($i=0; $i < 6; $i++) { 
+//     // echo $i;
+//     echo $nama[$i]."<br>";
+// }
+
+foreach ($nama as $key) {
+    echo $key."<br>";
+}
+
+// Array asosiatif
+
+$nama =array(
+    "satu"  => "sepuluh",
+    "dua"   => "dua puluh",
+    "tiga"  => "tiga puluh",
+    "empat" => "empat puluh"
+);
+
+var_dump($nama);
+
+echo "<br>";
+
+echo $nama['dua'];
+
+echo "<br>";
+
+foreach ($nama as $key => $value) {
+    echo $key." => ".$value;
+    echo "<br>";
+    ------------------
+}

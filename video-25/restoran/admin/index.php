967c09ec2cfda6ff418b581c44b6c0ac8b59f4c3
NikhoC1 <110102550+NikhoC1@users.noreply.github.com>
2022-11-06 17:24:29 +0700

    Add files via upload

---
+++ video-25/restoran/admin/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+    <title>Admin Pages | Aplikasi Restoran</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+        <div class="col-md-3">
+            <h2>Restoran</h2>
+        </div>
+        <div class="col-md-9">
+            <div class="float-end mt-4">logout</div>
+        </div>
+        </div>
+        <div class="row mt">
+            <div class="col-md-3">
+            <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="#">Kategori</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Menu</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Pelanggan</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Order</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Order Detail</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">User</a></li>
+            </ul>
+            </div>
+            <div class="col-md-9">
+
+            </div>
+        </div>
+        <div class="row mt-5">
+        <div class="col">
+            <p class="text-center">1996 - copyright@Tokko.com</p>
+        </div>
+        </div>
+    </div>
+</body>
+</html>

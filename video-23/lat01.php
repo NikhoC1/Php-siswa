283399c5f0c2889923df66fd2bbef298da4aa123
NikhoC1 <110102550+NikhoC1@users.noreply.github.com>
2022-11-06 17:21:40 +0700

    Add files via upload

---
+++ video-23/lat01.php
@@ -0,0 +1,9 @@
+<?php
+echo "Belajar php";
+echo '<br>';
+echo 'Saya Siswa smk';
+echo '<h1 style ="background:blue">Belajar php itu mudah</h1>
+<p style="background-color:red">Saya suka belajar program</p>';
+echo 'Saya Belajar'.'php <br>';
+echo 'Tahun'. 2019;
+?>
